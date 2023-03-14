5b9eef5075e27e5e02acbf20b67e4143620b515c
unknown <al_jager1983@yahoo.com>
2023-03-14 19:51:01 +0800

    finalizing UX 2

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
-//submit button
+
 const submitbtn = document.getElementById("submitbtn");
 const number = document.getElementById("numb");
-// const num = document.getElementsByClassName("num");
 const thankState = document.getElementById("thankstate");
 const rate = document.getElementById("rate");
 const selected = document.getElementById("selected");
 const reload = document.getElementById("reload");
 let i = 0;
-// let colorGrey = new Color ()
 let numbers = document.querySelectorAll(".num").length;
 
+
+//numbers selection
 for (var n=0; n<numbers; n++) {
     document.querySelectorAll(".num")[n].addEventListener("click", function() {
         let num = this.innerHTML;
         const numid = "num" + num;
         console.log(numid);
-        document.getElementById(numid).style.backgroundColor = "grey";
+        const colornumid = document.getElementById(numid).getAttribute("class");
+        console.log(colornumid);
+    //     if(document.getElementById(numid).style.backgroundColor === "#252d37"){
+    //         console.log("true");
+    //         // document.getElementById(numid).style.backgroundColor = "grey";
+    // } else {
+    //     console.log("false")
+    // }
         i++;
     })
 }
-//reload page, can be deleted later
 
+//reload page, can be deleted later(added UX)
 reload.addEventListener("click", function () {
     location.reload();
 })
 
+//submitbutton in Rating
 submitbtn.addEventListener("click", function() {
     selected.innerHTML = "You selected " + i + " out of 5";
     thankState.style.display = "grid";
     rate.style.display = "none";
 });
+
+const clearConsole = () => {
+    console.clear();
+}
+
+const clearConsoleTime = setTimeout(clearConsole, 2000);
